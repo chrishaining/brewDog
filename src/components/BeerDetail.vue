41b1cfd2a3cf331389f695c81f12f6e32055b035
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-12 14:21:53 +0100

    Beer details added

---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+<div v-if="beer" id="beer-detail">
+  <p>{{beer.name}}</p>
+  <img :src="beer.image_url" :alt="beer.name">
+  <p>Tagline: {{beer.tagline}}</p>
+  <p>Description: {{beer.description}}</p>
+</div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+
+export default {
+  name: "beer-detail",
+  props: ["beer"]
+}
+</script>
+
+<style lang="css" scoped>
+
+#beer-detail {
+  border: 2px solid black;
+  background-color: lightskyblue;
+  text-align: center;
+}
+
+img {
+  height: 100px;
+  width: 30px;
+}
+
+</style>
---
+++ src/components/BeerFilterForm.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <select v-on:change="handleSelect" v-model="selectedBeer">
+      <option value="" disabled selected>Select a beer...</option>
+      <option v-for="beer in beers" :value="beer">{{beer.name}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+
+export default {
+  name: "beer-filter-form",
+  data(){
+    return {
+      "selectedBeer": {}
+    }
+  },
+  props: ["beers"],
+  methods: {
+    handleSelect(){
+      eventBus.$emit("beer-selected", this.selectedBeer)
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
